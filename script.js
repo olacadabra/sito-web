86d38364d9faec4d0b7d16cc87457f39f3cd8897
olacadabra <41647964+olacadabra@users.noreply.github.com>
2021-01-31 23:06:45 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var smooth = [$('a.smooth'), 100, 750];
-smooth[0].click(function() {
-   $('html, body').animate({
-      scrollTop: $('[id="' + $.attr(this, 'href').substr(1) + '"]').offset().top -smooth[1]
-   }, smooth[2]);
-   return false;
-});
